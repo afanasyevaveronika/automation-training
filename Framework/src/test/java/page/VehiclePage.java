8960ce140e622af25d6afa07469a5e0099e9ad5a
v.afanasyeva08 <v.afanasyeva08@gmail.com>
2019-12-10 17:31:37 +0300

    add Framework

---
+++ Framework/src/test/java/page/VehiclePage.java
@@ -0,0 +1,42 @@
+package page;
+
+import test.java.page.AbstractPage;
+import test.java.page.MainPage;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class VehiclePage extends AbstractPage {
+    private static final Logger logger = LogManager.getLogger(MainPage.class);
+    private final String VEHICLE_PAGE_URL = "https://www.avis.com/en/reservation#/vehicles";
+
+    public VehiclePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(xpath = "//div[@id='vehicleTeaser']//div[@class='location-info']")
+    private WebElement pickUpLocationInfo;
+
+
+    @Override
+    public AbstractPage openPage() {
+        driver.navigate().to(VEHICLE_PAGE_URL);
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS);
+        logger.log(Level.INFO, "Vehicle page opened");
+        return this;
+    }
+
+    public String pickUpLocationInfo() {
+        return pickUpLocationInfo.getText();
+    }
+
+
+}
